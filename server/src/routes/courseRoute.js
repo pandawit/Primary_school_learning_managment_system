e787ec4fefb2516838f3d99c4c906d82f20149c1
pandawit <117365901+pandawit@users.noreply.github.com>
2024-04-06 20:46:54 -1200

    gg

--- server/src/routes/courseRoute.js
+++ server/src/routes/courseRoute.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 router.post('/submitCourse', courseController.submitCourse); 
-
 module.exports = router;
